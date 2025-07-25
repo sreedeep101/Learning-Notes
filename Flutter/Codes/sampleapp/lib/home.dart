1605d9adfcdcf537b6564281e4818e9563f50e87
sreedeep101 <pradeepantkmalappattam@gmail.com>
2025-07-25 07:31:40 +0530

    sample app compleated

--- Flutter/Codes/sampleapp/lib/home.dart
+++ Flutter/Codes/sampleapp/lib/home.dart
@@ -16,8 +16,7 @@ class Home extends StatelessWidget {
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
-          ),
-
+        ),
       ),
       body: Column(
         children: [
@@ -25,7 +24,7 @@ class Home extends StatelessWidget {
             alignment: Alignment.center,
             child: Column(
               children: [
-                SizedBox(height: 6,),
+                SizedBox(height: 6),
                 Text(
                   'TutorialKart',
                   style: TextStyle(
@@ -34,43 +33,74 @@ class Home extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(height: 10,),
-                Text(
-                  'Sign in',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 7,),
+                SizedBox(height: 10),
+                Text('Sign in', style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(height: 7),
                 SizedBox(
                   width: 400,
                   height: 50,
                   child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'User Name',
-                    border: OutlineInputBorder(),
+                    decoration: InputDecoration(
+                      labelText: 'User Name',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-                ),
-                SizedBox(height: 5,),
-                 SizedBox(
+                SizedBox(height: 5),
+                SizedBox(
                   width: 400,
                   height: 50,
                   child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-                ),
-                 SizedBox(height: 5,),
-                 Text(
+                SizedBox(height: 5),
+                Text(
                   'Forget Password',
                   style: TextStyle(
                     color: Colors.blue,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
+                SizedBox(height: 10),
+                SizedBox(
+                  width: 400,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      print('sucsussfully done');
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                 Row(   
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Dont have an account?',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            'Sign Up',
+                            style: TextStyle(fontSize: 15, color: Colors.blue),
+                          ),
+                        ],
+                      ),
               ],
             ),
           ),
